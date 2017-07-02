9c6b44dab579cbf5d30fa4df65d6b7eaee618052
Vic <vic@VicdeMacBook-Pro.local>
2017-07-02 23:37:38 +0800

    权限组相关接口

---
+++ app/controller/admin/powerGroup.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = app => {
+  class RoleController extends app.Controller {
+    * create() {
+      const { ctx } = this;
+      const createRule = {
+        data: {
+          type: 'object',
+          rule: {
+            name: { type: 'string', min: 2, max: 30 },
+          },
+        },
+      };
+      // 校验参数
+      ctx.validate(createRule);
+      const { data } = ctx.request.body;
+      const success = yield ctx.service.admin.addRole(data.name);
+      if (success) {
+        ctx.body = ctx.helper.returnSuccess({ data: { success: 1 } });
+      } else {
+        ctx.body = ctx.helper.returnError(2003);
+      }
+    }
+    * update() {
+      const { ctx } = this;
+      const createRule = {
+        data: {
+          type: 'object',
+          rule: {
+            name: { type: 'string', max: 30, min: 2 },
+          },
+        },
+      };
+      // 校验参数
+      ctx.validate(createRule);
+      const { data } = ctx.request.body;
+      const id = ctx.params.id;
+      const success = yield ctx.service.admin.updateRole(id, data.name);
+      if (success) {
+        ctx.body = ctx.helper.returnSuccess({ data: { success: 1 } });
+      } else {
+        ctx.body = ctx.helper.returnError(2004);
+      }
+    }
+    * destroy() {
+      const { ctx } = this;
+      const id = ctx.params.id;
+      const success = yield ctx.service.admin.deleteRole(id);
+      if (success) {
+        ctx.body = ctx.helper.returnSuccess({ data: { success: 1 } });
+      } else {
+        ctx.body = ctx.helper.returnError(2005);
+      }
+    }
+  }
+  return RoleController;
+};
